c38898393903dbc4e2eded04f5455846e218da9c
moranpatrick <patrickmoran121@gmail.com>
2018-03-29 18:14:43 +0100

    Myo Connected and unity reading x and y values from it

--- RollerGame/Assets/Scripts/PlayerController.cs
+++ RollerGame/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pose = Thalmic.Myo.Pose;
 
 public class PlayerController : MonoBehaviour {
+
+    public GameObject myo = null;
+    private Pose _lastPose = Pose.Unknown;
     public float speed;
     public Text countText;
     private Rigidbody rb;
@@ -20,14 +24,48 @@ public class PlayerController : MonoBehaviour {
 
     private void FixedUpdate()
     {
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+     
+        var JointObject = GameObject.Find("Myo");
+        float x = JointObject.transform.rotation.eulerAngles.x;
+        float y = JointObject.transform.rotation.eulerAngles.y;
+        Debug.Log("X IS: " + x);
+        Debug.Log("Y IS: " + y);
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
+        if (thalmicMyo.pose != _lastPose)
+        {
+            _lastPose = thalmicMyo.pose;
+            if (thalmicMyo.pose == Pose.WaveOut)
+            {
+                
+            }
+            else if(thalmicMyo.pose == Pose.WaveIn)
+            {
+                
+            }
+            else if (thalmicMyo.pose == Pose.DoubleTap)
+            {
+                
+            }
+            else if (thalmicMyo.pose == Pose.FingersSpread)
+            {
+                
+            }
+            else if (thalmicMyo.pose == Pose.Fist)
+            {
+            
+            }
+        }
+        else
+        {
+            
+        }
+
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
         rb.AddForce(movement * speed);
-
-    }
+    }//FixedUpdate()
 
     private void OnTriggerEnter(Collider other)
     {
